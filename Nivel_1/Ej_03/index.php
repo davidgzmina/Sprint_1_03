72ffda969c0b89d150c0a43364056a87e5b5b923
davidgzmina <davidmobile28@gmail.com>
2025-10-02 17:59:05 +0200

    Exercise 3 level 1 fixed logic

--- Nivel_1/Ej_03/index.php
+++ Nivel_1/Ej_03/index.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sprint_01_tema_03_ej_03</title>
 
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: cadetblue;
-            font-size: 30px;
-            color: white;
-        }
-    </style>
-</head>
-<body>
 <?php
 /*
 Crea una función que reciba como parámetros un array de palabras y un carácter. La función debe devolver true
@@ -23,32 +7,42 @@ Por ejemplo:
 Si tenemos ["hola", "Php", "Html"], devolverá true si preguntamos por "h", pero false si preguntamos por "l".
 */
 
-$palabras = ["merengue","colacao","cubata","cacatua"];
+$words = ["Lola","Paola","Lolo"];
 
-$letra = "c";
+$letter = "a";
 
 
-function buscarLetra($palabras,$letra) {
-    foreach($palabras as $palabra){
-        if(strpos($palabra,$letra)===false){
+function letterSearcher($words,$letter){
+
+    foreach($words as $word){
+
+        $found = false;
+
+        for ($i = 0; $i < strlen($word); $i++){
+            if($word[$i] == $letter){
+                $found = true;
+                break;
+            }
+        }
+
+        if($found == false){
             return false;
         }
-        else{
-            return true;
-        }
     }
+
+    return true;
 }
 
-$resultado = buscarLetra($palabras,$letra);
 
-if($resultado===true){
-    echo "Todas las palabras del array contienen la letra $letra.<br>";
+$resultado = letterSearcher($words,$letter);
+
+
+if ($resultado==true){
+    echo "Todas las palabras del array contienen la letra $letter.";
 }
 else{
-    echo "No todas las palabras del array contienen la letra $letra.";
+    echo "No todas las palabras del array contienen la letra $letter.";
 }
+ 
 
-?>
 
-</body>
-</html>
